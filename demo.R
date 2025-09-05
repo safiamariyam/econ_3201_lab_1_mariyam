2ea2d5d1a3e35bc848b1d6405e280d8e95cda994
safiamariyam <ksafia2003@gmail.com>
2025-09-05 15:06:37 -0500

    Add mpg plot

--- demo.R
+++ demo.R
@@ -1,1 +1,18 @@
 rm(list=ls())
+
+install.packages("ggplot2")
+install.packages("dplyr")
+library(ggplot2)
+library(dplyr)
+# Calculate the average highway mpg for each class of vehicle
+avg_mpg <- mpg %>%
+  group_by(class) %>%
+  summarise(avg_hwy_mpg = mean(hwy, na.rm = TRUE))
+# Plot the average highway mpg with class on the y-axis, all bars in light blue
+ggplot(avg_mpg, aes(y = class, x = avg_hwy_mpg)) +
+  geom_col(fill = "lightblue") + # Set all bars to light blue
+  labs(title = "Average Highway MPG by Vehicle Class",
+       y = "Vehicle Class",
+       x = "Average Highway MPG") +
+  theme_minimal()
+
